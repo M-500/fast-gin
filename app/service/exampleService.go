846ae17ab837e2018ed23ebe04b51ef8e3ed39bf
wulinlin <18574945291@163.com>
2023-12-04 18:07:21 +0800

    service层代码

--- app/service/exampleService.go
+++ app/service/exampleService.go
@@ -1,7 +1,28 @@
 package service
 
+import (
+	"context"
+	"fast-gin/app/dao"
+)
+
 //
 // @Description
 // @Author 代码小学生王木木
 // @Date 2023/12/4 11:50
 //
+
+type ExampleService struct {
+	ctx           context.Context
+	daoExampleDao *dao.ExampleDao
+}
+
+func NewExampleService(ctx context.Context) *ExampleService {
+	return &ExampleService{
+		ctx:           ctx,
+		daoExampleDao: dao.NewExampleDao(ctx),
+	}
+}
+
+func (s *ExampleService) Get(id int) {
+
+}
